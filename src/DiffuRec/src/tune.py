42e4b62962de0802a0ee4f146ad9be4884fedf95
Daniela Pekerskaya <elyasup@yandex.ru>
2026-04-15 01:29:45 +0300

    upd tune.py (DiffuRec)

--- src/DiffuRec/src/tune.py
+++ src/DiffuRec/src/tune.py
@@ -8,22 +8,17 @@ import logging
 from argparse import Namespace
 from functools import partial
 
-# Импорт из существующих файлов (не изменяем их)
 from main import load_and_split_gts, item_num_create, fix_random_seed_as
 from model import create_model_diffu, Att_Diffuse_model
 from utils import Data_Train, Data_Val, Data_Test
 from trainer import model_train, evaluate_and_print
 from evaluate_topk_dp import compute_all_metrics
 
-# Настройка логирования для Optuna (будет выводиться в консоль)
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 def compute_recall10_on_validation(model, val_loader, args):
-    """
-    recall@10 на валидационном наборе для заданной модели, нужен
-    после обучения для оценки качества гиперпараметров
-    """
+    """recall@10 на валидации """
     device = args.device
     model.eval()
     all_actual = []
@@ -33,20 +28,14 @@ def compute_recall10_on_validation(model, val_loader, args):
             batch = [x.to(device) for x in batch]
             _, rep_diffu, _, _, _, _ = model(batch[0], batch[1], train_flag=False)
             scores = model.diffu_rep_pre(rep_diffu)
-            _, topk = torch.topk(scores, k=10, dim=-1)   # только k=10
+            _, topk = torch.topk(scores, k=10, dim=-1)
             for i in range(len(batch[1])):
                 all_actual.append([batch[1][i].item()])
                 all_predicted.append(topk[i].cpu().tolist())
-    #  метрики только для k=10
     _, recalls, _, _, _ = compute_all_metrics(all_actual, all_predicted, [10], args.item_num)
-    return recalls[0]   # Recall@10
+    return recalls[0]
 
-def objective(trial, base_args, tra_data_loader, val_data_loader, test_data_loader):
-    """
-    Целевая функция для Optuna.
-    Предлагает гиперпараметры, обучает модель и возвращает Recall@10 на валидации.
-    """
-    # базовый конфиг
+def objective(trial, base_args, data_raw):
     lr = trial.suggest_float('lr', 1e-4, 1e-2, log=True)
     batch_size = trial.suggest_categorical('batch_size', [256, 512, 1024])
     hidden_size = trial.suggest_categorical('hidden_size', [64, 128, 256])
@@ -58,7 +47,6 @@ def objective(trial, base_args, tra_data_loader, val_data_loader, test_data_load
     noise_schedule = trial.suggest_categorical('noise_schedule', ['trunc_lin', 'linear', 'cosine'])
     schedule_sampler_name = trial.suggest_categorical('schedule_sampler_name', ['uniform', 'lossaware'])
 
-    # Копируем базовые параметры и обновляем гиперпараметрами
     args = Namespace(**vars(base_args))
     args.lr = lr
     args.batch_size = batch_size
@@ -70,22 +58,18 @@ def objective(trial, base_args, tra_data_loader, val_data_loader, test_data_load
     args.lambda_uncertainty = lambda_uncertainty
     args.noise_schedule = noise_schedule
     args.schedule_sampler_name = schedule_sampler_name
-
-    # Уменьшаем количество эпох для тюнинга 
     args.epochs = 20
     args.eval_interval = 5
     args.patience = 3
 
     fix_random_seed_as(args.random_seed)
 
-    # Пересоздаём загрузчики с новым batch_size 
-    tra_data = Data_Train(tra_data_loader.u2seq, args)  
-    val_data = Data_Val(val_data_loader.u2seq, val_data_loader.u2answer, args)
-    test_data = Data_Test(test_data_loader.u2seq, test_data_loader.u2seq_add, test_data_loader.u2answer, args)
-
+    tra_data = Data_Train(data_raw['train'], args)
+    val_data = Data_Val(data_raw['val_seq'], data_raw['val'], args)
+    test_data = Data_Test(data_raw['test_seq'], {uid: [] for uid in data_raw['test_seq']}, data_raw['test'], args)
     tra_loader = tra_data.get_pytorch_dataloaders()
     val_loader = val_data.get_pytorch_dataloaders()
-    test_loader = test_data.get_pytorch_dataloaders()   # нужен для model_train
+    test_loader = test_data.get_pytorch_dataloaders()
 
     diffu_rec = create_model_diffu(args)
     model = Att_Diffuse_model(diffu_rec, args)
@@ -103,11 +87,25 @@ def main():
         max_len=50,
         device='cuda' if torch.cuda.is_available() else 'cpu',
         num_gpu=1,
-        metric_ks=[10],       
+        batch_size=512,               
+        hidden_size=128,
+        dropout=0.1,
+        emb_dropout=0.3,
+        num_blocks=4,
+        diffusion_steps=32,
+        lambda_uncertainty=0.001,
+        noise_schedule='trunc_lin',
+        schedule_sampler_name='lossaware',
         optimizer='Adam',
+        lr=0.001,
         weight_decay=0,
         momentum=None,
         rescale_timesteps=True,
+        metric_ks=[10],               # только k=10 
+        eval_interval=20,
+        patience=5,
+        epochs=500,                   
+        description='Diffu_norm_score',
         long_head=False,
         diversity_measure=False,
         epoch_time_avg=False,
@@ -117,26 +115,13 @@ def main():
     data_raw = load_and_split_gts(quantiles=(0.7, 0.8, 0.9))
     base_args = item_num_create(base_args, len(data_raw['smap']))
 
-
-    tra_data = Data_Train(data_raw['train'], base_args)
-    val_data = Data_Val(data_raw['val_seq'], data_raw['val'], base_args)
-    test_data = Data_Test(data_raw['test_seq'], {uid: [] for uid in data_raw['test_seq']}, data_raw['test'], base_args)
-
-    tra_loader = tra_data.get_pytorch_dataloaders()
-    val_loader = val_data.get_pytorch_dataloaders()
-    test_loader = test_data.get_pytorch_dataloaders()
-
     study = optuna.create_study(direction='maximize', study_name='diffurec_tuning')
-    objective_partial = partial(objective, base_args=base_args,
-                                tra_data_loader=tra_data,
-                                val_data_loader=val_data,
-                                test_data_loader=test_data)
-    study.optimize(objective_partial, n_trials=15, timeout=3600*1)  # 15 trials или 1 час
+    objective_partial = partial(objective, base_args=base_args, data_raw=data_raw)
+    study.optimize(objective_partial, n_trials=15, timeout=3600)  # 15 trials или 1 час
 
     best_params = study.best_params
     print("Best hyperparameters:", best_params)
 
-    #  Финальное обучение с лучшими параметрами 
     final_args = Namespace(**vars(base_args))
     for key, value in best_params.items():
         setattr(final_args, key, value)
@@ -144,7 +129,6 @@ def main():
     final_args.eval_interval = 20
     final_args.patience = 5
 
-    # Пересоздаём загрузчики с финальным batch_size
     tra_data_final = Data_Train(data_raw['train'], final_args)
     val_data_final = Data_Val(data_raw['val_seq'], data_raw['val'], final_args)
     test_data_final = Data_Test(data_raw['test_seq'], {uid: [] for uid in data_raw['test_seq']}, data_raw['test'], final_args)
@@ -159,8 +143,7 @@ def main():
 
     best_model_final, _ = model_train(tra_loader_final, val_loader_final, test_loader_final, model_final, final_args, logger)
 
-    # Baseline и adaptation инференс
-    print("\n Final evaluation ")
+    print("\nFinal evaluation ")
     baseline_test_seq = {}
     for uid in data_raw['test_seq'].keys():
         full_seq = data_raw['test_seq'][uid]
